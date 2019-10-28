b7f17f3df250a274b8ad72c66bd1d39200d96b15
lolgame854 <55812052+lolgame854@users.noreply.github.com>
2019-10-29 00:01:15 +0100

    Fichiers

--- commands/admin/listwarn.js
+++ commands/admin/listwarn.js
@@ -16,11 +16,11 @@ module.exports = {
     if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send(noperm)
         let member = message.mentions.members.first()
         if (!member) return message.channel.send(embed)
-        let embed = new Discord.RichEmbed()
+        let embed2 = new Discord.RichEmbed()
         .setColor("PURPLE")
             .setAuthor(member.user.username, member.user.displayAvatarURL)
             .addField('10 derniers warns', ((warns[member.id] && warns[member.id].length) ? warns[member.id].slice(0, 10).map(e => e.reason) : "Ce membre n'a aucun warns"))
             .setTimestamp()
-        message.channel.send(embed)
+        message.channel.send(embed2)
   }
 };
