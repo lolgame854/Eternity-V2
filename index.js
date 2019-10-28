dd20db727c740ece7481133e4871f9376b85e229
lolgame854 <55812052+lolgame854@users.noreply.github.com>
2019-10-28 22:45:34 +0100

    Fichiers

--- index.js
+++ index.js
@@ -24,6 +24,21 @@ config({
   require(`./handlers/${handler}`)(client);
 });
 
+client.on('ready', () => {
+  console.log(`${client.user.username} est connecté !`)
+
+    let status = [
+        `${client.guils.size} serveur`,
+        `${client.user.size} membre`,
+        `${client.channel.size} channel`,
+        `e! => e!help`
+    ]
+    setInterval(function() {
+        let statu = status[Math.floor(Math.random() * status.length)];
+        client.user.setActivity(status, {type: "WATCHING"})
+    }, interval);
+})
+
 client.on("message", async message => {
   const prefixe = "e!"
 
