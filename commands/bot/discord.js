492f5a283c67841afe5bd4e09039d6345dbde1c5
lolgame854 <55812052+lolgame854@users.noreply.github.com>
2019-10-29 09:36:16 +0100

    Fichiers

--- commands/bot/discord.js
+++ commands/bot/discord.js
@@ -5,8 +5,10 @@ module.exports = {
   description: "",
   run: async (client, message, args) => {
     const embed = new Discord.RichEmbed()
+    .setColor("PURPLE")
     .setAuthor("Serveur Discord")
     .setTitle("Rejoindre")
+    .setURL('https://discord.gg/msF3hKk')
     .setDescription("Click sur rejoindre pour rejoindre le serveur discord !\n \nMarci si tu le fait.")
     .setFooter("Eternity © 2019 Un problème ? Demandes à lolgame854#3470 !")
     message.channel.send(embed)
