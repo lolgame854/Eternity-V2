c95bd9aed579a413a2339bac7fb08b85d7f0107e
lolgame854 <55812052+lolgame854@users.noreply.github.com>
2019-10-31 13:58:20 +0100

    Fichiers

---
+++ commands/bot/github.js
@@ -0,0 +1,15 @@
+const Discord = require('discord.js')
+module.exports = {
+  name: "github",
+  category: "bot",
+  description: "",
+  run: async (client, message, args) => {
+    const embed = new Discord.RichEmbed()
+    .setAuthor("lolgame854#3470")
+    .setColor("PURPLE")
+    .setTitle("GitHub")
+    .setURL('https://github.com/lolgame854/SourceCode')
+    .setDescription("Voici le github d'un bot que j'ai créé. ^^")
+    .setFooter("Eternity © 2019 Un problème ? Demandes à lolgame854#3470 !")
+  }
+};
