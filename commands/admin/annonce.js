e6103151d90b31edea27f59ac19d8858a4d0faed
lolgame854 <55812052+lolgame854@users.noreply.github.com>
2019-11-01 21:39:07 +0100

    Fichiers

--- commands/admin/annonce.js
+++ commands/admin/annonce.js
@@ -13,13 +13,18 @@ module.exports = {
     .setColor("PURPLE")
     .setTitle("Annonce")
     .setDescription("Veuillez mettre un message a la suite")
+    const embed2 = new Discord.RichEmbed()
+    .setColor("PURPLE")
+    .setTitle("Annonce")
+    .setDescription("Votre message a bien été envoyer !")
     const args1 = message.content.trim().split(/ +/g)
     const saymsg = args1.slice(1).join(" ")
 
     message.delete()
     if (!message.member.hasPermission("MANAGE_MESSAGES"))
-    return message.channel.send(noperm);
+    return message.channel.send(noperm).then(m => m.delete(2000));
   if(!args[0]) return message.channel.send(embed);
+  message.chanel.send(embed2).then(m => m.delte(2000))
   message.guild.members.forEach(member => {
     member.send(`Annonce envoyer par : ${message.author.tag}\n \n${saymsg}`).catch(e => {});
   })
